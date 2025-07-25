4cf51b44da4e1c8b4f6092fab4bed18005235cdc
Samuel Waller <samcolton17@gmail.com>
2025-07-25 00:40:46 -0500

    more work done

--- printNova.js
+++ printNova.js
@@ -29,6 +29,28 @@ function printInitFact (stack, fact, ruleStartFlag) {
 		console.log(`   :${stack}: ${fact.join(' ')}`)
 	}
 }
+
+
+
+function printInitFacts (stacks) {
+	let firstFact = true
+	if (stacks['@include']) {
+		for (const fact of stacks['@include']) {
+			printInitFact('@include', fact, firstFact)
+			firstFact = false
+		}
+		delete stacks['@include']
+	}
+	console.log()
+	firstFact = true
+	for (const stack in stacks) {
+		for (const fact of stacks[stack]) {
+			printInitFact(stack, fact, firstFact)
+		}
+		firstFact = false
+	}
+}
+
 function printRule (rule) {
 	let out = '|'
 	for (const cause of rule.causes) {
@@ -49,3 +71,4 @@ function printRule (rule) {
 
 printNova.printRule = printRule;
 printNova.printInitFact = printInitFact;
+printNova.printInitFacts = printInitFacts;
